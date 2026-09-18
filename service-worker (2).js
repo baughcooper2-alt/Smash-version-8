61c430d52e89ed546e1b1d724b47607352354ae2
baughcooper2-alt <baughcooper2@gmail.com>
2026-09-17 20:28:43 -0400

    Add files via upload

---
+++ service-worker (2).js
@@ -0,0 +1,34 @@
+const CACHE = 'smash-tracker-v3';
+const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png', './roster-seed.json'];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    caches.match(e.request).then((hit) => {
+      if (hit) {
+        fetch(e.request).then((res) => {
+          if (res && res.ok) caches.open(CACHE).then((c) => c.put(e.request, res.clone()));
+        }).catch(() => {});
+        return hit;
+      }
+      return fetch(e.request)
+        .then((res) => {
+          if (res && res.ok) caches.open(CACHE).then((c) => c.put(e.request, res.clone()));
+          return res;
+        })
+        .catch(() => caches.match('./index.html'));
+    })
+  );
+});
